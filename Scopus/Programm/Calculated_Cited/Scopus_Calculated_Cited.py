fd93570350294454042980b1a0a8fe540518699c
Ku-Gaskar <ku@aver-s.com.ua>
2023-01-10 14:27:30 +0200

    Realise

--- Scopus/Programm/Calculated_Cited/Scopus_Calculated_Cited.py
+++ Scopus/Programm/Calculated_Cited/Scopus_Calculated_Cited.py
@@ -2,25 +2,29 @@
 from tkinter import filedialog
 import tkinter.messagebox as mb
 import sys
-
-import os.path
 import re
-
-
+import os
 
 
 #***************************************************************************************************
 
 
-Path_file_bib=filedialog.askopenfilename(title='Откройте \"scopus...bib\"',filetypes=[('bib file','*.bib')])
-if not Path_file_bib: 
-  mb.showwarning("Ошибка","Файл *.bib не выбран. \n Программа завершит работу")
+folder_selected = filedialog.askdirectory(title='Откройте папку с файлами \"scopus...bib\"')
+if not folder_selected: 
+  mb.showwarning("Ошибка","папка *.bib не выбрана. \n Программа завершит работу")
   sys.exit()
+#os.path.dirname(Path_file_bib)
+arr = os.listdir(folder_selected)
+res=0
+for Path_file_bib in arr:
+    if Path_file_bib[-3:] != 'bib':continue
+    Bib_file = open(folder_selected+'/'+Path_file_bib,'r',encoding='UTF-8')
+    buff=Bib_file.read()
+    Bib_file.close()
 
-Bib_file = open(Path_file_bib,'r',encoding='UTF-8')
-buff=Bib_file.read()
-Bib_file.close()
+    find_Cited=re.findall(r'{cited By ([\d]*)}', buff)
+    res+=sum(int(i) for i in find_Cited )
 
-At=buff.split('@')
+print('Суммарное количество цитированний: %d' %res)
 
 sys.exit()
