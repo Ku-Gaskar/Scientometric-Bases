18fb8bc2ca85b0e93031b28e92fbc7e39dfa9142
Ku-Gaskar <ku@aver-s.com.ua>
2023-02-04 17:00:37 +0200

    1

--- Postgres/creat_table_WOS_update.py
+++ Postgres/creat_table_WOS_update.py
@@ -12,34 +12,80 @@ LIST_columns=('Unique-ID','Title','Journal','Year','Author','Volume','Number','P
               'Publisher','Type')
 LIST_id=('ResearcherID-Numbers','ORCID-Numbers')
 #----------------------------------------------------------------
+
 def data_preparation(one_autor:str):
-    def find_one(one_,name:str):
+    #----------------------------------------------------------------
+    def find_one(one_:str,name:str):
         #request=f"^{name} = "+chr(92)+"{([\s\S]+?*)\}"
-        start=one_.rfind('\n'+name)
-        f_str=re.findall("^"+name+"\{([\s\S]+?*)\}",one_)
-        return f_str[0].replace('\n','').replace('  ',' ') if f_str else ''
+        patern='\n'+name+' = {'
+        start=one_.find(patern)
+        if start==-1: return ''
+        start+=len(patern)
+        end=one_.find('}',start)
+        return one_[start:end].replace('   ',' ').replace('  ',' ').replace('\n','')
+    #----------------------------------------------------------------
+        #f_str=re.findall("^"+name+r"\{([\s\S]+?*)\}",one_)
+        #return f_str[0].replace('\n','').replace('  ',' ') if f_str else '
     data=[]    
     for name in LIST_columns:
         data.append(find_one(one_autor,name))
-    return data, find_one(one_autor,LIST_id[0]), find_one(one_autor,LIST_id[0])
+
+    researcher_dict=dict(re.findall(r'([\w]+, [\w]+).{0,}?/([A-Za-z]{1,}-[0-9]{3,}-[0-9]{4})',find_one(one_autor,LIST_id[0])))
+    orcid_dict=dict(re.findall(r'([\w]+, [\w]+).{0,}?/([0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9X]{4})',find_one(one_autor,LIST_id[1])))
+    
+    return data,researcher_dict,orcid_dict
 
 #********************************************************************
 def update_wos(db_conect,one_autor):
+    def search_in_table_hnure_for_author(author,cursor,orcid):
+        if orcid:
+            cursor.execute("""SELECT "id_Sciencer" FROM public."Table_Sсience_HNURE" AS tsh WHERE tsh."ORCID_ID" = %s;""",(orcid,))
+            res=cursor.fetchall()
+            if res: return res
+        
+        cursor.execute("""SELECT * FROM public.lat_name_hnure AS lnh WHERE lnh.name_lat = %s;""",(author,))
+        return cursor.fetchall()   
+    #----------------------------------------------------------------
     data1=data_preparation(one_autor)
     data, r_id, r_orcid = data1
     cursor = db_conect.cursor()
-    cursor.execute("""INSERT INTO public.scopus AS t(eid,title,journal,year,volume,number,pages,doi,note,publisher,document_type,author) 
-            SELECT * FROM (values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)) v(eid,title,journal,year,volume,number,pages,doi,note,publisher,document_type,author) 
-            WHERE NOT EXISTS  (SELECT FROM public.scopus AS d where d.eid = v.eid) 
-            on conflict do nothing returning "eid";""",tuple(data))
+    cursor.execute("""INSERT INTO public.wos AS t(unique_id,title,journal,year,author,volume,number,pages,doi,note,publisher,document_type) 
+            SELECT * FROM (values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)) v(unique_id,title,journal,year,author,volume,number,pages,doi,note,publisher,document_type) 
+            WHERE NOT EXISTS  (SELECT FROM public.wos AS d where d.unique_id = v.unique_id) 
+            on conflict do nothing returning "unique_id";""",tuple(data))
     res=cursor.fetchone()
-    if res: Id_Aticl=res[0]
+    if res: 
+        Id_Aticl=res[0]        
+        for author in data[4].split(" and "):
+            author_orcid,author_r_id='',''
+            if author in r_orcid: author_orcid=r_orcid[author]   
+            if author in r_id: author_r_id=r_id[author]
+            author_id_hnure=search_in_table_hnure_for_author(author,cursor,author_orcid) 
     else: 
-        cursor.execute("""UPDATE public.scopus AS s SET note = %s, data_update = now()
-                            WHERE  s.eid = %s 
-                            RETURNING  s.eid;""",(data[8],data[0]))
-        Id_Aticl=cursor.fetchone()[0]
+#test----------------------------------------------------------------
+        for author in data[4].split(" and "):
+            author_orcid,author_r_id='',''
+            if author in r_orcid: author_orcid=r_orcid[author]   
+            if author in r_id: author_r_id=r_id[author]
+            author_id_hnure=search_in_table_hnure_for_author(author,cursor,author_orcid)
+            if  len(author_id_hnure) > 1:
+                print(f"ERROR: more than 1 -> {len(author_id_hnure)} :\n{author_id_hnure}")
+                
+                author_id_hnure=int(author_id_hnure[0][0])
+            elif len(author_id_hnure)==1:
+                author_id_hnure=int(author_id_hnure[0][0])
+            else: author_id_hnure=None
 
+            cursor.execute("""INSERT INTO public.wos_autors AS t(unique_id,orcid,researcher_id,author,id_autor) 
+            SELECT unique_id,orcid,researcher_id,author,id_autor::int FROM (values (%s,%s,%s,%s,%s)) v(unique_id,orcid,researcher_id,author,id_autor) 
+            WHERE NOT EXISTS  (SELECT FROM public.wos_autors AS d where (d.unique_id = v.unique_id) and (d.author = v.author)) 
+            on conflict do nothing returning "id_autor";""",(data[0],author_orcid ,author_r_id,author,author_id_hnure))
+
+#test----------------------------------------------------------------
+        cursor.execute("""UPDATE public.wos AS s SET note = %s, data_update = now()
+                            WHERE  s.unique_id = %s 
+                            RETURNING  s.unique_id;""",(data[9],data[0]))
+        Id_Aticl=cursor.fetchone()[0]
     print (Id_Aticl)
     return
 
